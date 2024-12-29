baafe14889123aece17b7f3912e4cc53aac0ea78
Madhukar Gumanju <madhugumangu@gmail.com>
2024-12-29 23:08:21 +0545

    dsn

---
+++ src/constants/strings.ts
@@ -0,0 +1,38 @@
+class Strings {
+  static telegram = "Telegram";
+  static telegramLink = "https://telegram.me/madhukargumanju";
+  static telegramUsername = "madhukargumanju";
+
+  static github = "GitHub";
+  static githubLink = "https://www.github.com/madhukargumanju";
+  static githubUsername = "madhukargumanju";
+
+  static linkedIn = "LinkedIn";
+  static linkedInLink = "https://www.linkedin.com/in/madhukargumanju";
+  static linkedInUsername = "madhukargumanju";
+
+  static instagram = "Instagram";
+  static instagramLink = "https://www.instagram.com/madhukargumanju";
+  static instagramUsername = "madhukargumanju";
+
+  static twitter = "Twitter";
+  static twitterLink = "https://www.twitter.com/madhukargumanzu";
+  static twitterUsername = "madhukargumanju07";
+
+  static email = "Email";
+  static primaryEmailLink = "mailto:madhugumangu@gmail.com";
+  static primaryEmail = "madhugumangu@gmail.com";
+
+  static buyMeACoffee = "BuyMeACoffee";
+  static buyMeACoffeeLink = "https://buymeacoffee.com/madhukargumanju";
+  static buyMeACoffeeUsername = "madhukargumanju";
+
+  static koFi = "Ko-Fi";
+  static koFiLink = "https://ko-fi.com/madhukargumanju";
+  static koFiUsername = "madhukargumanju";
+
+  static fullName = "Madhukar Gumanju";
+  static shortName = "madhukargumanju";
+}
+
+export default Strings;
